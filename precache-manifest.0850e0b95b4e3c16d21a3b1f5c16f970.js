0b409bd15f736f7bf1d1900978e2c8e6fe0d06e8
Hassan-jr <abdiladifhassan115@gmail.com>
2022-04-10 07:07:37 +0300

    Updates

---
+++ precache-manifest.0850e0b95b4e3c16d21a3b1f5c16f970.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = [
+  {
+    "revision": "2a26c54ace6051e026f7",
+    "url": "/Caske/static/css/main.0a2364ac.chunk.css"
+  },
+  {
+    "revision": "2a26c54ace6051e026f7",
+    "url": "/Caske/static/js/main.2a26c54a.chunk.js"
+  },
+  {
+    "revision": "26ddafddc6cdadae872d",
+    "url": "/Caske/static/js/1.26ddafdd.chunk.js"
+  },
+  {
+    "revision": "ff6523d8119f49697153",
+    "url": "/Caske/static/js/2.ff6523d8.chunk.js"
+  },
+  {
+    "revision": "82ee78cebbd229f7597c",
+    "url": "/Caske/static/js/runtime~main.82ee78ce.js"
+  },
+  {
+    "revision": "8c0be211eee7b696118780192802610b",
+    "url": "/Caske/static/media/img2.8c0be211.jpg"
+  },
+  {
+    "revision": "c5c4a8c5c2bd4f6b922cd72bf2dbf674",
+    "url": "/Caske/static/media/img1.c5c4a8c5.jpg"
+  },
+  {
+    "revision": "41f31bfaf1d2321ba510534e9365d52e",
+    "url": "/Caske/static/media/img18.41f31bfa.jpg"
+  },
+  {
+    "revision": "e42b0a7bef0ea6c526b8d430e7c13843",
+    "url": "/Caske/static/media/img3.e42b0a7b.jpg"
+  },
+  {
+    "revision": "d913ee0b45bc8bb11ce56f7b2a17ab8a",
+    "url": "/Caske/static/media/img20.d913ee0b.jpg"
+  },
+  {
+    "revision": "dd75a1ed75642ff92753a3dd0847b0cc",
+    "url": "/Caske/static/media/img4.dd75a1ed.jpg"
+  },
+  {
+    "revision": "c5ecf7ead37828ab50a652e2f5314cbf",
+    "url": "/Caske/static/media/img5.c5ecf7ea.jpg"
+  },
+  {
+    "revision": "a2fe3e7c408996c6dcf521d140e9b86e",
+    "url": "/Caske/static/media/img6.a2fe3e7c.jpg"
+  },
+  {
+    "revision": "0036a4b580fae906a767d013bc6019bf",
+    "url": "/Caske/static/media/img7.0036a4b5.jpg"
+  },
+  {
+    "revision": "4d0793d5fcb4ca9e23256e5aab6dcc31",
+    "url": "/Caske/static/media/img8.4d0793d5.jpg"
+  },
+  {
+    "revision": "07bd0274f4799060244814a56a3f7553",
+    "url": "/Caske/static/media/img9.07bd0274.jpg"
+  },
+  {
+    "revision": "171d11c57789874603313f8fece558e5",
+    "url": "/Caske/static/media/img10.171d11c5.jpg"
+  },
+  {
+    "revision": "d72182e25fa6f3b425086f17b5a750d3",
+    "url": "/Caske/static/media/img11.d72182e2.jpg"
+  },
+  {
+    "revision": "c04871e38e67dadea5e484967557c4eb",
+    "url": "/Caske/static/media/img12.c04871e3.jpg"
+  },
+  {
+    "revision": "38eac53f8248d96cb9b396c6cef073c3",
+    "url": "/Caske/static/media/img17.38eac53f.jpg"
+  },
+  {
+    "revision": "38eac53f8248d96cb9b396c6cef073c3",
+    "url": "/Caske/static/media/img19.38eac53f.jpg"
+  },
+  {
+    "revision": "1fb2290c3129b4107ef56e6ac8365b9c",
+    "url": "/Caske/static/media/img21.1fb2290c.jpg"
+  },
+  {
+    "revision": "e594dfc16967185dd978b279d490e910",
+    "url": "/Caske/static/media/img22.e594dfc1.jpg"
+  },
+  {
+    "revision": "7f051aac00f8f1698406b37572a8bfe0",
+    "url": "/Caske/static/media/img23.7f051aac.jpg"
+  },
+  {
+    "revision": "36a7b4d603a62e2ec7a614774ade6a69",
+    "url": "/Caske/static/media/img24.36a7b4d6.jpg"
+  },
+  {
+    "revision": "f468cda4f11e55717c9421b531a6fd2a",
+    "url": "/Caske/static/media/img13.f468cda4.jpg"
+  },
+  {
+    "revision": "8d306ee140c4d7291e74e90a90e44116",
+    "url": "/Caske/static/media/img14.8d306ee1.jpg"
+  },
+  {
+    "revision": "f0f2b18d2f63f100c38e078dab05bc90",
+    "url": "/Caske/static/media/img15.f0f2b18d.jpg"
+  },
+  {
+    "revision": "72a15ebf1522607934cc90fd217b6ee1",
+    "url": "/Caske/static/media/img16.72a15ebf.jpg"
+  },
+  {
+    "revision": "9554ff5278d4cc78022b62071e18ed32",
+    "url": "/Caske/index.html"
+  }
+];
